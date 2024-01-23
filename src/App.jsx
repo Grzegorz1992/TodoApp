95cd03e06fddaa8fe0256828022223b92f8613f1
Grzegorz <gkopacki92@gmail.com>
2024-01-23 23:48:27 +0100

    styled header element in App.module.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import styles from "./App.module.css";
 
 function App() {
-	return <div className={styles.container}>
-   <header>
-    <h1>Do zrobienia</h1>
-    <h2>5 zadań</h2>
-    <button>+</button>
-   </header>
-  </div>;
+	return (
+		<div className={styles.container}>
+			<header className={styles.header}>
+				<div>
+					<h1>Do zrobienia</h1>
+					<h2>5 zadań</h2>
+				</div>
+				<button>+</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
